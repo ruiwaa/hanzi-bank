8200a74b1e466041c6cdd8c34cf5d2982c62f3bb
ruiwaa <yeejii08@gmail.com>
2026-06-19 09:58:11 +0900

    feat: 로그인/회원가입 닫기 버튼 추가

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,16 @@
+import { X } from "lucide-react";
+import Link from "next/link";
+
 export default function LoginLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-[#F9FAFB]">
+    <main className="relative min-h-screen flex items-center justify-center bg-[#F9FAFB]">
+      <Link href={"/"} className="absolute left-6 top-6">
+        <X aria-label="현재 페이지 닫기" />
+      </Link>
       {children}
     </main>
   );
