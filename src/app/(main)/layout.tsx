cea76ae707620f39e5d247a89489175af46d0b6e
ruiwaa <yeejii08@gmail.com>
2026-06-24 16:53:06 +0900

    refactor: sidebar, sidebarMobile Suspense 로 감싸기

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -13,29 +13,27 @@ export default function MainLayout({
   return (
     <div className="max-w-full min-h-screen desktop-layout">
       <h1 className="sr-only">중단어 창고 메인페이지</h1>
-      <aside className="sidebar border-r border-border bg-white min-h-screen">
-        <Link href={"/"}>
-          <Image
-            src="/logo2.png"
-            alt={"중단어 창고 로고"}
-            width={200}
-            height={200}
-            className="hidden lg:block px-4 w-full"
-          />
-        </Link>
-        <Sidebar />
-      </aside>
-      <div className="flex flex-col min-w-0">
-        <div className="bg-white border-b border-border p-2">
-          <Header />
+      <Suspense fallback={<div className="animate-pulse">로딩 중...</div>}>
+        <aside className="sidebar border-r border-border bg-white min-h-screen">
+          <Link href={"/"}>
+            <Image
+              src="/logo2.png"
+              alt={"중단어 창고 로고"}
+              width={200}
+              height={200}
+              className="hidden lg:block px-4 w-full"
+            />
+          </Link>
+          <Sidebar />
+        </aside>
+        <div className="flex flex-col min-w-0">
+          <div className="bg-white border-b border-border p-2">
+            <Header />
+          </div>
+          <main className="flex-1 dark:bg-black p-3">{children}</main>
+          <SidebarMobile />
         </div>
-        <main className="flex-1 dark:bg-black p-3">
-          <Suspense fallback={<div className="animate-pulse">로딩 중...</div>}>
-            {children}
-          </Suspense>
-        </main>
-        <SidebarMobile />
-      </div>
+      </Suspense>
     </div>
   );
 }
