c828a7c9b1ddc0f9ba81df9143c3a27c2efb5842
ruiwaa <yeejii08@gmail.com>
2026-06-25 20:24:28 +0900

    style: 급수별 단어 리스트 호버 스타일링 적용

--- src/app/(main)/hsk-level-words/[level]/components/DetailHskLevelList.tsx
+++ src/app/(main)/hsk-level-words/[level]/components/DetailHskLevelList.tsx
@@ -1,30 +1,36 @@
 import { HskLevelWords } from "@/app/api/fetchAllHskLevelWords";
 import SaveWordBtn from "@/components/ui/SaveWordBtn";
 import { POS_KO_MAP } from "@/constants/posToKo";
+import Link from "next/link";
 
 interface Props {
   words: HskLevelWords;
 }
 
-export default function DetailHskLevelList({ words }: Props) {
+export default async function DetailHskLevelList({ words }: Props) {
   return (
     <>
       {words.words.map((word) => (
         <li
           key={word.id}
-          className=" border-b py-2 h-20  text-[12px] md:whitespace-nowrap md:text-[14px] border-gray-300 mb-2 grid grid-cols-[1fr_1fr_2fr_1fr] md:grid-cols-[1fr_1fr_2fr_1fr_1fr_1fr] items-center justify-items-center gap-2"
+          className=" hover:bg-taupe-100 border-b py-3 h-25 text-[12px]  md:text-[16px] border-gray-300 grid grid-cols-[1fr_1fr_2fr_1fr] md:grid-cols-[1fr_1fr_2fr_1fr_1fr_1fr] items-center justify-items-center gap-2 "
         >
-          <h3 className="font-chinese">{word.word}</h3>
-          <span className="text-muted-foreground">[ {word.pinyin} ]</span>
-          <span className="md:whitespace-normal md:break-keep text-center">
-            {word.meanings
-              .map((meaning) => meaning.ko.replace(/\s*;\s*/g, ", "))
-              .join(", ")}
-          </span>
-          <span className="hidden md:break-keep whitespace-normal md:block text-center">
-            {word.pos.map((pos) => POS_KO_MAP[pos] ?? pos).join(", ")}
-          </span>
-          <span className=" hidden md:block">{word.hsk_level} 급</span>
+          <Link
+            href={`/hsk-level-words/${word.hsk_level}/${word.id}`}
+            className="contents"
+          >
+            <h3 className="font-chinese">{word.word}</h3>
+            <span className="text-muted-foreground">[ {word.pinyin} ]</span>
+            <span className="md:whitespace-normal md:break-keep text-center">
+              {word.meanings
+                .map((meaning) => meaning.ko.replace(/\s*;\s*/g, ", "))
+                .join(", ")}
+            </span>
+            <span className="hidden md:break-keep whitespace-normal md:block text-center">
+              {word.pos.map((pos) => POS_KO_MAP[pos] ?? pos).join(", ")}
+            </span>
+            <span className=" hidden md:block">{word.hsk_level} 급</span>
+          </Link>
           <SaveWordBtn />
         </li>
       ))}
