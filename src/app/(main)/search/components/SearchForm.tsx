31c7db920e9e6e656439e86bead82f10ba70afce
ruiwaa <yeejii08@gmail.com>
2026-06-29 14:14:09 +0900

    feat: 텍스트 검색 유효성 검사 추가

--- src/app/(main)/search/components/SearchForm.tsx
+++ src/app/(main)/search/components/SearchForm.tsx
@@ -1,10 +1,34 @@
-import BackButton from "@/components/ui/BackButton";
+import { FieldErrors, UseFormRegister } from "react-hook-form";
 
-export default function SearchForm() {
+interface Props {
+  register: UseFormRegister<{
+    keyWord: string;
+  }>;
+  errors: FieldErrors<{
+    keyWord: string;
+  }>;
+}
+
+export default function SearchForm({ register, errors }: Props) {
   return (
-    <div>
-      <BackButton page={"/"} />
-      <span>모달창</span>
+    <div className="flex flex-col gap-1">
+      <h3 className="text-muted-foreground">텍스트 검색</h3>
+      <label htmlFor="textSearch" className="sr-only">
+        텍스트 검색
+      </label>
+      <input
+        id="textSearch"
+        placeholder="찾으시는 단어를 검색해주세요."
+        aria-describedby="textSearchHelp"
+        className="bg-input p-2 rounded-xl"
+        {...register("keyWord")}
+      />
+      {errors.keyWord && (
+        <p className="text-red-500">{errors.keyWord.message}</p>
+      )}
+      <span id="textSearchHelp" className="sr-only">
+        찾으시는 단어를 입력해주세요.
+      </span>
     </div>
   );
 }
--- src/components/ui/SearchModal.tsx
+++ src/components/ui/SearchModal.tsx
@@ -1,22 +1,48 @@
 "use client";
 
+import { searchSchema, searchValue } from "@/app/(auth)/schemas/searchSchemas";
+import SearchForm from "@/app/(main)/search/components/SearchForm";
 import { useSearchModal } from "@/stores/searchModalStore";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { X } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 
 export default function SearchModal() {
   const { isOpen, close } = useSearchModal();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<searchValue>({ resolver: zodResolver(searchSchema) });
+  const router = useRouter();
+  const onSubmit = (data: searchValue) => {
+    close();
+    router.push(`/search=${data.keyWord}`);
+  };
   if (!isOpen) return null;
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center "
+      role="dialog"
+      aria-modal="true"
+      className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs"
       onClick={close}
     >
       <div
-        className="w-full bg-amber-200 rounded-2xl p-5 shadow-xl"
+        className="w-full md:m-auto lg:max-w-5xl bg-white rounded-2xl p-5 shadow-xl"
         onClick={(e) => e.stopPropagation()}
       >
-        <div>
-          <h2>단어 검색</h2>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-row md:justify-between">
+            <h2 className="m-auto lg:m-0 font-bold">단어 검색</h2>
+            <button onClick={close} aria-label="검색창 닫기">
+              <X />
+            </button>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <SearchForm register={register} errors={errors} />
+          </form>
         </div>
       </div>
     </div>
