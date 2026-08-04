340b44e0edcab1e247a1c37af5b76b930487ad3d
ruiwaa <yeejii08@gmail.com>
2026-08-04 11:50:33 +0900

    feat: 나의 단어 정보 퍼블리싱 완료

--- src/app/(main)/mypage/myWord/components/MyWordCard.tsx
+++ src/app/(main)/mypage/myWord/components/MyWordCard.tsx
@@ -1,14 +1,36 @@
+import SoundButton from "@/components/ui/SoundButton";
 import { MyWord } from "@/types/DBTypes";
+import Link from "next/link";
 
 interface Props {
   word: MyWord;
 }
 
 export default function MyWordCard({ word }: Props) {
+  const myWord = word.hsk_words;
   return (
-    <li>
-      <p>{word.hsk_words.word}</p>
-      <p>{word.hsk_words.pinyin}</p>
+    <li className="border-b border-b-gray-100 last:border-b-0">
+      <div className="p-2 flex flex-col gap-1">
+        <div className="flex flex-row gap-2 items-center">
+          <Link
+            href={`/hsk-level-words/${word.hsk_words.hsk_level}/${myWord.id}`}
+          >
+            <h2 className="text-lg font-extrabold">{myWord.word}</h2>
+          </Link>
+          <span className="bg-blue-100 text-primary p-1 rounded-lg self-center text-sm text-center font-semibold">
+            HSK {myWord.hsk_level}급{" "}
+          </span>
+          <SoundButton
+            text={myWord.word}
+            ariaLabel={`${myWord.meanings[0].ko} 중국어 발음 듣기`}
+            size={20}
+            width={25}
+            height={25}
+          />
+        </div>
+        <p className="text-gray-500 text-md">[{word.hsk_words.pinyin}]</p>
+        <p className="text-sm">{myWord.meanings[0].ko}</p>
+      </div>
     </li>
   );
 }
--- src/app/(main)/mypage/myWord/components/MyWordSection.tsx
+++ src/app/(main)/mypage/myWord/components/MyWordSection.tsx
@@ -3,13 +3,19 @@
 import { useMyWords } from "@/hooks/useMywords";
 import { useSession } from "@/hooks/useSession";
 import MyWordCard from "./MyWordCard";
+import { Loader2Icon } from "lucide-react";
 
 export default function MyWordSection() {
   const { user } = useSession();
   const { data: myWords = [], isLoading } = useMyWords(user?.id);
 
   if (isLoading) {
-    return <p className="mx-auto">불러오는 중...</p>;
+    return (
+      <div className="flex flex-col">
+        <Loader2Icon className="animate-spin" />
+        <p> 불러오는 중...</p>
+      </div>
+    );
   }
 
   return (
@@ -17,7 +23,7 @@ export default function MyWordSection() {
       {myWords.length === 0 ? (
         <p>저장된 단어가 없습니다.</p>
       ) : (
-        <ul className="mt-4 space-y-4">
+        <ul className="mt-4 space-y-4 bg-white p-4 rounded-2xl border border-gray-200">
           {myWords.map((item) => (
             <MyWordCard key={item.hsk_words.id} word={item} />
           ))}
