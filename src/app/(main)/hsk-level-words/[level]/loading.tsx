8890c3de6dfa372cc2091c03b21184b444267a05
ruiwaa <yeejii08@gmail.com>
2026-08-12 09:33:59 +0900

    style: 로딩 스피너 높이 조정

--- src/app/(main)/hsk-level-words/[level]/loading.tsx
+++ src/app/(main)/hsk-level-words/[level]/loading.tsx
@@ -1,3 +1,10 @@
+import { Loader2 } from "lucide-react";
+
 export default function Loading() {
-  return <div>로딩 중...</div>;
+  return (
+    <div className="flex flex-col min-h-1/2 items-center justify-center gap-2">
+      <Loader2 className="animate-spin" />
+      <p>로딩 중...</p>
+    </div>
+  );
 }
