765bdf4e256d67301d1b52a0fd9c25e299dcebe6
ruiwaa <yeejii08@gmail.com>
2026-08-19 13:28:30 +0900

    feat: 구글 소셜 컴포넌트 추가

---
+++ src/app/(auth)/components/LoginToGoogle.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+interface Props {
+  onCLick: () => void;
+}
+
+export default function LogintoGoogle({ onCLick }: Props) {
+  return (
+    <button
+      type="button"
+      onClick={onCLick}
+      className="w-full rounded-lg flex flex-row gap-2 items-center justify-center border border-primary hover:bg-accent dark:border-blue-600 dark:text-blue-600"
+    >
+      <Image
+        src={"/images/lightGoogle.svg"}
+        alt={"구글 로고"}
+        width={30}
+        height={30}
+        className="block dark:hidden"
+      />
+      <Image
+        src={"/images/darkGoogle.svg"}
+        alt={"구글 로고"}
+        width={30}
+        height={30}
+        className="hidden dark:block"
+      />
+      <span className="font-medium">Google로 로그인</span>
+    </button>
+  );
+}
